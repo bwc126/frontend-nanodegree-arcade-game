db11785ef21acc59a7fb7049b1c6f2aa8786e6b2
Brian Coe <bwc126@gmail.com>
2015-10-16 21:04:19 -0400

    feat: Add prototypal functions for player object

--- js/app.js
+++ js/app.js
@@ -24,7 +24,21 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(input) {
+  
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
